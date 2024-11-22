9d227ff752f6b0e66476251321ad09a85be4a09f
Rohrhurst_Marco <32276415+Yuraken@users.noreply.github.com>
2024-11-22 21:37:34 +0100

    Init + installation de primevue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,46 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <FloatLabel>
+    <InputText id="username" v-model="value" />
+    <label for="username">Username</label>
+    </FloatLabel>
+    <Button label="Toggle Dark Mode" @click="toggleDarkMode()" />
+
   </div>
+
+
 </template>
 
 <script>
+import FloatLabel from 'primevue/floatlabel';
+import InputText from 'primevue/inputtext'; 
+import Button from 'primevue/button';
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
+  },
+  data() {
+    return {
+      // Initialisation du tableau 'value'
+      value: "test", // Exemple avec quelques éléments
+    };
+  },
+  components: {
+    FloatLabel, // Assurez-vous d'utiliser la bonne syntaxe
+    InputText,
+    Button
+  }, 
+  methods: {
+    toggleDarkMode() {
+      document.documentElement.classList.toggle('my-app-dark');
+}
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue'; // Importation de Vue 3
+import App from './App.vue'; // Le composant principal de votre application
+import PrimeVue from 'primevue/config'; // Configuration de PrimeVue
+import Material from '@primevue/themes/material';
 
-createApp(App).mount('#app')
+
+
+// Création de l'application Vue
+const app = createApp(App);
+
+// Utilisation de PrimeVue dans l'application
+app.use(PrimeVue,{
+    theme: {
+        preset: Material,
+        options: {
+            darkModeSelector: '.my-app-dark',
+        }
+    }
+});
+
+// Montage de l'application sur le div avec id "app"
+app.mount('#app');
